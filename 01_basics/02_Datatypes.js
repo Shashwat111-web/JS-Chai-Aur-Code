86124c2a7513e201b59ef32297e0e8ed5b3006cb
Shashwat <shashwatagrawal197@gmail.com>
2024-07-26 10:48:30 +0530

    DataTypes added

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,41 @@
+"use strict";//treat all Js code as newer version 
+
+/* how this line "use strict" is considered bydefault in the newer version */
+
+//alert(3+3) //we are using nodejs , not browser
+
+// documentation padhna seekho 
+
+// mdn preffered 
+
+// tc39 is the original documentation of the js however an easier documentation is made available by the mdn organization 
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript
+
+
+let name = "Shashwat"
+let age = 3
+let isLoggedIn = false
+let email;//undefined
+let temperature = null 
+
+
+
+
+// number = 2 to power 53
+// BigInt
+// string => ""
+// Boolean => true/false 
+// null => standalone value 
+// undefined => 
+// symbol => used to distinguish between elements majorly in react
+
+// object 
+
+console.log(typeof name)
+console.log(typeof age);
+
+// Important 
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
