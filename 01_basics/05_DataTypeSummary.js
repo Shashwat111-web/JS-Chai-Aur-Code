2bcae4ff9ece8764680c14d70da0053a82f8d5d1
Shashwat <shashwatagrawal197@gmail.com>
2024-07-26 12:15:10 +0530

    Summary Added

---
+++ 01_basics/05_DataTypeSummary.js
@@ -0,0 +1,39 @@
+// Primitives 
+
+//  7 types : String , Number , Boolean , null , undefined , BigInt, Symbol
+
+
+// Reference type or  Non primitive -
+// Arrays ,  objects , functions  
+
+//Array
+const heros = ["shaktiman" , "doga" , "nagraj"] // datatype :object
+
+// Object represented with curly braces 
+let myObj = {
+    name:"hitesh",
+    age:22,
+} // datatype : object 
+
+// function 
+const myFunction = function(){
+    console.log("Hello World");
+} //data type : function object 
+
+// Javascript two most important topics browser events and objects 
+
+
+// Return type of variables in JavaScript
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object
