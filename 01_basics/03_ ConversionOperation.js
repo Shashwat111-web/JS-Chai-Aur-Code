789f1841da60363e194fb4c4a339090ee55063aa
Shashwat <shashwatagrawal197@gmail.com>
2024-07-26 11:08:29 +0530

    Comparsion Operation Added

---
+++ 01_basics/03_ ConversionOperation.js
@@ -0,0 +1,36 @@
+let score = "hitesh"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); 
+
+/* Objects which cannot be converted into number are by default converted into NaN(Not  a number) whose data type is also a numebr */
+
+
+//"33" =>33
+// "33abc" =>NaN
+// true => 1 and false => 0
+
+let isLoggedIn = "hitesh";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn);
+
+
+// 1 => true ; 0 => false 
+//""=> false 
+// "hitesh" => true 
+
+
+let someNumber = 33
+
+let stringNumber = String(33)
+
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
